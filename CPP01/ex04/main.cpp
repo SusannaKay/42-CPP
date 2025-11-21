11b0d576b0e44b08d219c38df1a678fdb4e56cd9
Susanna Kayed <skayed@e3r1p5.42roma.it>
2025-11-21 15:18:19 +0100

    ex04 initial commit

---
+++ CPP01/ex04/main.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skayed <skayed@student.42roma.it>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/20 16:29:38 by skayed            #+#    #+#             */
+/*   Updated: 2025/11/21 15:07:47 by skayed           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main (int argc, char**argv){
+
+	std::string line;
+	std::string s1;
+	std::string s2;
+	std::string filename;
+	
+	if (argc != 4){
+		std::cout << "Usage: <filename> <s1> <s2>" << std::endl;
+		return 1;
+	}
+	filename = argv[1];
+	s1 = argv[2];
+	s2 = argv[3];		
+	if (s1.length() == 0)
+	{
+		std::cout << "s1 should not be empty";
+		return 1;
+	}
+	std::ifstream file(filename.c_str());
+	if(!file.is_open()){
+		std::cout << "Impossibile aprire il file" << std::endl;
+		return 1;
+	}
+	std::string outname = filename + ".replace";
+	std::ofstream new_file(outname.c_str());
+	if(!new_file.is_open()){
+		std::cout << "Errore new file" << std::endl;
+		return (1);
+	}
+
+	while (std::getline(file, line)){
+		std::string result;
+		size_t pos = 0;
+		size_t found = 0;	
+		while ((found = line.find(s1, found)) != std::string::npos)
+		{
+			result = line.substr(pos,found) + s2;
+			pos = found + s2.length();
+			found += s1.length();
+		}
+		new_file << result << std::endl;	
+	}
+}
+	
