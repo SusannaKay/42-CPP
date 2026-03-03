dde13ccc3f439755319ab1e9140e0b2409f6b677
Susanna Kayed <skayed@e3r2p2.42roma.it>
2026-03-03 16:23:51 +0100

    Finished ex03

--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -6,12 +6,26 @@
 /*   By: skayed <skayed@student.42roma.it>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 10:53:43 by skayed            #+#    #+#             */
-/*   Updated: 2026/03/02 15:46:49 by skayed           ###   ########.fr       */
+/*   Updated: 2026/03/03 16:21:22 by skayed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-bool bsp(Point const a, Point const b, Point const c, Point const point){
-	
+static Fixed	check_point(Point const a, Point const b, Point const p)
+{
+	return ((b.getx() - a.getx()) * (p.gety() - a.gety()) - (b.gety()
+			- a.gety()) * (p.getx() - a.getx()));
+}
+bool	bsp(Point const a, Point const b, Point const c, Point const point)
+{
+	Fixed d1(check_point(a, b, point));
+	Fixed d2(check_point(b, c, point));
+	Fixed d3(check_point(c, a, point));
+
+	if (d1 < 0 && d2 < 0 && d3 < 0)
+		return (true);
+	if (d1 > 0 && d2 > 0 && d3 > 0)
+		return (true);
+	return (false);
 }
--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -6,16 +6,25 @@
 /*   By: skayed <skayed@student.42roma.it>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 10:53:54 by skayed            #+#    #+#             */
-/*   Updated: 2026/03/02 15:45:05 by skayed           ###   ########.fr       */
+/*   Updated: 2026/03/03 16:18:48 by skayed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
-#include "Fixed.hpp"
 
-int main()
+bool	bsp(Point const a, Point const b, Point const c, Point const point); 
+
+int main(void)
 {
-	Point a(1.0f, 2.0f);
-	std::cout << a.getx() << " " << a.gety() << "\n";
-return 0;
+	Point a(0.f, 0.f);
+	Point b(10.f, 0.f);
+	Point c(0.f, 10.f);
+
+	Point p1(1.f, 1.f);   // dentro
+	Point p2(10.f, 10.f); // fuori
+	Point p3(0.f, 5.f);   // bordo -> fuori
+
+	std::cout << bsp(a, b, c, p1) << "\n";
+	std::cout << bsp(a, b, c, p2) << "\n";
+	std::cout << bsp(a, b, c, p3) << "\n";
 }
